e88bc6fdfb1e8aca81b44dea64ea8e1af4acf2b7
IsaacDevBr <123705069+IsaacDevBr@users.noreply.github.com>
2024-04-23 15:33:33 -0300

    implementando código do desafio do felipao da DIO e add titulo no arquivo html

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hello,World!")
+const nomeDeUsuario = "Deuso Programdor";
+let xp = 0;
+let nivel;
+
+
+if (xp >= 0 && xp <= 1000) {
+    nivel = "Ferro"
+} else if(xp >= 1001 && xp <= 2000) {
+    nivel = "Bronze"
+} else if(xp >= 2001 && xp <= 5000) {
+    nivel = "Prata"
+} else if(xp >= 5001 && xp <= 7000) {
+    nivel = "Ouro"
+} else if(xp >= 7001 && xp <= 8000) {
+    nivel = "Platina"
+} else if(xp >= 8001 && xp <= 9000) {
+    nivel = "Ascendente"
+} else if(xp >= 9001 && xp <= 10000) {
+    nivel = "Imortal"
+} else  {
+    nivel = "Radiante"
+}
+
+const mensagemFinal = `O herói de nome ${nomeDeUsuario} está no nível de ${nivel}`
+console.log(mensagemFinal);
